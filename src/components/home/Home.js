532a945c2126f555ee87aa9a6552675785ea4333
Raihan Kabir <raihancse46@gmail.com>
2021-04-25 19:01:15 +0600

    start project, add libraries, create folder structure & make Digital Experience component

---
+++ src/components/home/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import DigitalExperience from './DigitalExperience'
+
+export default function Home() {
+    return (
+        <>
+            <DigitalExperience />
+        </>
+    )
+}
