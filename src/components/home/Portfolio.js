21bbe2e465301950f2b16b2be03d45f18cbf5e0c
Raihan Kabir <raihancse46@gmail.com>
2021-05-02 16:02:22 +0600

    portfolio tab color change based on background color

--- src/components/home/Portfolio.js
+++ src/components/home/Portfolio.js
@@ -1,22 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Tab, Tabs } from 'react-bootstrap'
 import Content from './fortfolio/Content'
 import Tech from './fortfolio/Tech'
 import Business from './fortfolio/Business'
 
 export default function Portfolio() {
+    const [adc, setAdc] = useState()
+
+    const handleSelect = (eventKey) => {
+        if(eventKey=="tech") {
+            setAdc('ad')
+        }
+        else if(eventKey!=="tech") {
+            setAdc('')
+        }
+    }
+
     return (
         <>
             <section className="protfolio-section spb-100">
                 <div className="tab-wrapper">
-                    <Tabs className="rotate" defaultActiveKey="business" id="portfolio-tab">
-                        <Tab eventKey="content" title="Content" tabClassName="bgs-tab">
+                    <Tabs className="rotate" defaultActiveKey="business" id="portfolio-tab" onSelect={handleSelect}>
+                        <Tab eventKey="content" title="Content" tabClassName={`bgs-tab ${adc}`}>
                             <Content />
                         </Tab>
-                        <Tab eventKey="tech" title="Tech" tabClassName="">
+                        <Tab eventKey="tech" title="Tech" tabClassName={``}>
                             <Tech />
                         </Tab>
-                        <Tab eventKey="business" title="Business" tabClassName="bgs-tab">
+                        <Tab eventKey="business" title="Business" tabClassName={`bgs-tab ${adc}`}>
                             <Business />
                         </Tab>
                     </Tabs>
